139786713b06ac48f2a64b4c9d384d53269d6890
Jon <jonprosser84@gmail.com>
2025-09-14 21:09:44 +0100

    useRef to focus

--- src/components/TimerControls.jsx
+++ src/components/TimerControls.jsx
@@ -1,8 +1,18 @@
+import { useEffect, useRef } from "react";
 
-const TimerControls = ({toggleTimer, resetTime, isRunning}) => {
+const TimerControls = ({ toggleTimer, resetTime, isRunning }) => {
+  const startButtonRef = useRef(null);
+  useEffect(() => {
+    if(startButtonRef.current) {
+        startButtonRef.current.focus();
+    }
+
+  }, [])
+
   return (
     <div>
       <button
+        ref={startButtonRef}
         onClick={toggleTimer}
         className="mt-3 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-300 mr-3"
       >
